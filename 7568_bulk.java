1d4efb84a65c45f1ea300e8473cbbccde4999b0a
22000527shl <90017419+22000527sehyunlee@users.noreply.github.com>
2022-07-10 01:17:04 +0900

    Create 7568_bulk.java

---
+++ 7568_bulk.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        int[] w = new int[num];
+        int[] h = new int[num];
+        int[] result = new int[num];
+        int i,j;
+
+        for (i=0; i<num; i++){
+            w[i] = s.nextInt();
+            h[i] = s.nextInt();
+        }
+        
+        for (i=0; i<num; i++){
+            for (j=0; j<num; j++){
+                if(w[j] > w[i] && h[j]> h[i]) {
+                    result[i] ++;
+                }
+            }
+            System.out.printf("%d ", result[i]+1);
+        }
+
+        s.close();
+    }
+}
+        
